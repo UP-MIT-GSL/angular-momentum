cdbc5b0de2b47bcc6970a1976efe79fa74602159
Tim Joseph Dumol <tim@timdumol.com>
2013-04-17 02:53:33 +0800

    Added lots of comments to the PP file.

--- puppet/angular-momentum.pp
+++ puppet/angular-momentum.pp
@@ -1,10 +1,21 @@
+# This Puppet (https://puppetlabs.com/) file declares the system configuration
+# for the Vagrant system (you can also apply this manually using
+# `puppet apply --modulepath=./modules:./vendor_modules angular-momentum.pp`).
+
+# This declares a dependency on the apt class of the apt Puppet module
+# (https://forge.puppetlabs.com/puppetlabs/apt)
 class {'apt': 
+  # This configures apt to always update when provisioning
   always_apt_update => true
 }
 
+# This is an alternative syntax to depend on a class (in this case, the
+# nodejs class of the nodejs module). This differs from the above syntax
+# in that you can include a class multiple times without an error,
+# but you cannot supply parameters to include.
 include nodejs
 
-# The database
+# This defines the db class
 class db {
   class {'rethinkdb':
     instance_name => 'momentum',
@@ -12,4 +23,5 @@ class db {
   }
 }
 
+# This declares a dependency on the above defined db class
 class {'db':}
